9f640142d72cd34c0d822098de241d810863e9f7
DabenW <wangdaben@meituan.com>
2023-01-20 16:45:53 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-4g1w2ush39cb3b97","alias":"cloud1"}]
+const isMac = true
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.sh
@@ -0,0 +1,1 @@
+"/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e cloud1-4g1w2ush39cb3b97 --n quickstartFunctions --r --project "/Users/daben/WeChatProjects/miniprogram-1" --report_first --report
